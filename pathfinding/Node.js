d847186bb6897b3918c3cafba330055bdaa027ca
asJohannsson <Grandis_pizza@hotmail.com>
2014-02-19 21:13:27 +0100

    Try catch test
    Kan gjøres mer komplett, men funksjonaliteten til testen er god nok for
    koden som er skrevet så langt.

--- pathfinding/Node.js
+++ pathfinding/Node.js
@@ -7,14 +7,24 @@ function Node(id, x, y){
 	var prevNode = id;
 	//Legger til en nabo node(En kant som man kan gå på til å komme til neste node) 
 	this.addneighbor = function(type, id, cost){
+		try{ 
 		neighbors.push(new Edge(type, id, cost));	
+		}catch(addneighborerr){
+				console.log("Could not add neighbor type: " + type + " id: " + id + "cost : " +cost + "\n" + addneighborerr);
+		}
 	};
 	//setter "cost" for å komme til denne noden(Denne gangen algortimen blei kjørt, vist han 
 	//finner en path som var mer effektiv overskrives denne)
 	this.setOptimalCostPath = function(OptCost, nodeid){
-		if(this.getCost() > OptCost){
-			cost = OptCost;
-			setPrevNode(nodeid);
+		try{
+			if(this.getCost() > OptCost){
+				cost = OptCost;
+				setPrevNode(nodeid);
+				return true;
+			}
+			return false;
+		}catch(setOptCostPatherr){
+			console.log(setOptCostPatherr);
 		}
 	};
 	//Setter kordinatene til noden, tenkt brukt i sammenheng med kartene.
@@ -33,8 +43,12 @@ function Node(id, x, y){
 	};
 	//Metode som endrer alle kantene til noden til enten å være i "handicap mode" eller omvendt.
 	this.handicap = function(){
-		for(var edgenr = 0; edgenr<neighbors.length; edgenr++){
-			neighbors[edgenr].handicapedge();
+		try{
+			for(var edgenr = 0; edgenr<neighbors.length; edgenr++){
+				neighbors[edgenr].handicapedge();
+			}
+		}catch(nodehandicaperr){
+			console.log(nodehandicaperr);
 		}
 	};
 	//Henter inn kordinatene til noden.
@@ -51,12 +65,16 @@ function Node(id, x, y){
 	};
 	this.toString = function(){
 		return "This nodes id is " + id + "\n" + cords.toString() + "and the current cost to get to the node is " + cost + "\n";	
-	}
+	};
 	this.printneighbors = function(){
 		var neighedges = "Edges:\n";
-		for(var len = 0; len < neighbors.length; len++){
-			neighedges += neighbors[len].toString() + "\n";		
+		try{
+			for(var len = 0; len < neighbors.length; len++){
+				neighedges += neighbors[len].toString() + "\n";		
+			}
+		}catch(printneighborerr){
+			console.log(printneighborerr);
 		}
 		return neighedges;
-	}
+	};
 }
